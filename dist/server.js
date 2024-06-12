cb3dc0bdea9bc84d4700daaf9cad55687e4ac9b9
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-06-12 21:27:48 +0600

    new Endpoints Added

--- dist/server.js
+++ dist/server.js
@@ -41,7 +41,9 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const cors_1 = __importDefault(require("cors"));
 const nearAPI = __importStar(require("near-api-js"));
+const near_api_js_1 = require("near-api-js");
 const { keyStores } = nearAPI;
+const { Contract } = nearAPI;
 const myKeyStore = new keyStores.InMemoryKeyStore();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true, limit: '10mb' }));
@@ -63,10 +65,28 @@ const mainConfig = {
     helperUrl: 'https://helper.mainnet.near.org',
     explorerUrl: 'https://nearblocks.io',
 };
+//"account_id":"splaunch.testnet","public_key":"ed25519:5aisAqwijq1segvM9LxABeo9iEGVg8nJKZrn3Pcq8HSX","private_key":"ed25519:UvQcP2QzWxguaqSCpcNJv6tCVL52sBZeySoFHMEsGYgYZ2gzJHnMXe93dGRoSp7vJjLxssvfrx6pEAzP1YvWUoH"
+//ed25519:bMc9FpDZqiCzEXovFcPnWjMwggErxboRMiUkmtL4t2EutykFr6ZnHVA6RzgvRF8VuwYpU7141PPqB7HnPjtF1kA
+let contract;
+let account;
 const setup = () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("setup");
+    const PRIVATE_KEY = "ed25519:UvQcP2QzWxguaqSCpcNJv6tCVL52sBZeySoFHMEsGYgYZ2gzJHnMXe93dGRoSp7vJjLxssvfrx6pEAzP1YvWUoH"; // Directly use the private key
+    const keyPair = near_api_js_1.KeyPair.fromString(PRIVATE_KEY);
+    // console.log('KeyPair:', keyPair); // Debug: print the key pair
+    yield myKeyStore.setKey('testnet', 'splaunch.testnet', keyPair);
     const near = yield connect(testConfig);
+    const account = yield near.account("splaunch.testnet");
+    const methodOptions = {
+        viewMethods: ['ft_balance_of', 'get_greeting'],
+        changeMethods: ['ft_transfer', 'set_greeting'],
+        useLocalViewExecution: true
+    };
+    contract = new Contract(account, "splaunch.testnet", methodOptions);
+    console.log("Setup Done");
     return near;
 });
+setup();
 app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.send('Hello World');
 }));
@@ -84,6 +104,107 @@ app.get('/balance', (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     catch (error) {
         console.log(error);
         res.status(500).send({ error });
+    }
+}));
+app.get('/get_greeting', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const greeting = yield contract['get_greeting']();
+        res.json({ greeting });
+    }
+    catch (error) {
+        res.status(500).json({ error: error });
+    }
+}));
+app.get('/spearbalance', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.header('Access-Control-Allow-Origin', 'https://ashikurrahman25.github.io'); // Allow your specific origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    try {
+        const { account_id } = req.body;
+        console.log(account_id);
+        const balance = yield contract['ft_balance_of']({
+            account_id: account_id
+        });
+        res.json({ balance });
+    }
+    catch (error) {
+        res.status(500).json({ error: error });
+    }
+}));
+app.get('/getAllPlayerPoints', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const playerPoints = yield contract['getAllPlayerPoints']();
+        res.json({ playerPoints });
+    }
+    catch (error) {
+        res.status(500).json({ error: error });
+    }
+}));
+app.post('/set_greeting', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { greeting } = req.body;
+        if (!greeting) {
+            return res.status(400).json({ error: 'Missing "greeting" in the request body.' });
+        }
+        // Call the contract's set_greeting method
+        const result = yield contract['set_greeting']({ greeting });
+        console.log(result);
+        res.json({ success: true, message: "Successfully updated greeting message!" });
+    }
+    catch (error) {
+        res.status(500).json({ error: "New " + error.message });
+    }
+}));
+app.post('/claim', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.header('Access-Control-Allow-Origin', 'https://ashikurrahman25.github.io'); // Allow your specific origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    try {
+        console.log(req.body);
+        const { receiver_id, amount, memo } = req.body;
+        if (!receiver_id) {
+            return res.status(400).json({ error: 'Missing "greeting" in the request body.' });
+        }
+        const near = yield connect(testConfig);
+        const acc = yield near.account('splaunch.testnet');
+        const functionCallResult = yield acc.functionCall({
+            contractId: 'splaunch.testnet',
+            methodName: 'ft_transfer',
+            args: {
+                receiver_id,
+                amount,
+                memo
+            },
+            gas: BigInt(100000000000000), // attached GAS (optional)
+            attachedDeposit: BigInt(1) // attached deposit in yoctoNEAR (optional)
+        });
+        // Call the contract's set_greeting method
+        // const result = await (contract as any)['ft_transfer'](
+        // {
+        //   receiver_id: receiver_id,
+        //   amount: amount,
+        //   memo:memo
+        // }
+        // ,"10000000000000"
+        // ,"1"
+        // );
+        // // Extract transaction hash from the function call result
+        // const transactionHash = functionCallResult.transaction.hash;
+        // // Extract transaction status using the transaction hash
+        // const transactionStatus = await near.connection.provider.txStatus(transactionHash, acc.accountId);
+        // // Extract and log the contract execution logs
+        // const logs = transactionStatus.receipts_outcome.flatMap(outcome => outcome.outcome.logs);
+        // // Log the transaction receipt
+        // const receipt = transactionStatus.transaction_outcome;
+        // return {
+        //   logs,
+        //   receipt,
+        // };
+        console.log(functionCallResult.transaction_outcome.id);
+        res.json({ success: true, message: "Successfully claimed reward!", txnLink: `https://testnet.nearblocks.io/txns/${functionCallResult.transaction_outcome.id}` });
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
     }
 }));
 const port = process.env.PORT || 9000;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express';
+import express, { raw } from 'express';
 const app = express();
 import cors from 'cors';
 import * as nearAPI from 'near-api-js';
+import { FinalExecutionOutcome } from 'near-api-js/lib/providers/provider';
+import { KeyPair, utils } from 'near-api-js';
 const { keyStores } = nearAPI;
+const { Contract } = nearAPI;
 const myKeyStore = new keyStores.InMemoryKeyStore();
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
@@ -32,21 +37,51 @@ const mainConfig = {
   explorerUrl: 'https://nearblocks.io',
 };
 
+
+//"account_id":"splaunch.testnet","public_key":"ed25519:5aisAqwijq1segvM9LxABeo9iEGVg8nJKZrn3Pcq8HSX","private_key":"ed25519:UvQcP2QzWxguaqSCpcNJv6tCVL52sBZeySoFHMEsGYgYZ2gzJHnMXe93dGRoSp7vJjLxssvfrx6pEAzP1YvWUoH"
+//ed25519:bMc9FpDZqiCzEXovFcPnWjMwggErxboRMiUkmtL4t2EutykFr6ZnHVA6RzgvRF8VuwYpU7141PPqB7HnPjtF1kA
+let contract: nearAPI.Contract;
+let account;
 const setup = async () => {
 
+  console.log("setup");
+  const PRIVATE_KEY = "ed25519:UvQcP2QzWxguaqSCpcNJv6tCVL52sBZeySoFHMEsGYgYZ2gzJHnMXe93dGRoSp7vJjLxssvfrx6pEAzP1YvWUoH"; // Directly use the private key
+  const keyPair = KeyPair.fromString(PRIVATE_KEY);
+  // console.log('KeyPair:', keyPair); // Debug: print the key pair
+  await myKeyStore.setKey('testnet', 'splaunch.testnet', keyPair);
+  
   const near = await connect(
     testConfig
   );
+
+  
+
+  const account = await near.account("splaunch.testnet");
+  const methodOptions = {
+    viewMethods: ['ft_balance_of', 'get_greeting'],
+    changeMethods: ['ft_transfer', 'set_greeting'],
+    useLocalViewExecution: true 
+  };
+
+  contract = new Contract(account,"splaunch.testnet",
+  methodOptions
+);
+console.log("Setup Done");
+
   return near;
+
+
 };
 
+
+ setup();
 app.get('/', async (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/balance', async (req, res) => {
 
-  res.header('Access-Control-Allow-Origin', 'https://ashikurrahman25.github.io'); // Allow your specific origin
+    res.header('Access-Control-Allow-Origin', 'https://ashikurrahman25.github.io'); // Allow your specific origin
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
@@ -59,6 +94,142 @@ app.get('/balance', async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send({ error });
+  }
+});
+
+app.get('/get_greeting', async (req, res) => {
+  try {
+  const greeting = await (contract as any)['get_greeting']()
+    res.json({ greeting });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.get('/spearbalance', async (req, res) => {
+
+
+  res.header('Access-Control-Allow-Origin', 'https://ashikurrahman25.github.io'); // Allow your specific origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  try {
+    const { account_id } = req.body;
+    console.log(account_id)
+  const balance = await (contract as any)['ft_balance_of']({
+    account_id:account_id
+  })
+    res.json({ balance });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+
+
+app.get('/getAllPlayerPoints', async (req, res) => {
+  try {
+  const playerPoints = await (contract as any)['getAllPlayerPoints']()
+    res.json({ playerPoints });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.post('/set_greeting', async (req, res) => {
+  try {
+
+    const { greeting } = req.body;
+
+    if (!greeting) {
+      return res.status(400).json({ error: 'Missing "greeting" in the request body.' });
+    }
+
+    // Call the contract's set_greeting method
+    const result = await (contract as any)['set_greeting']({ greeting });
+
+
+    console.log(result);
+
+    res.json({ success: true, message: "Successfully updated greeting message!" });
+
+  } catch (error: any) {
+    res.status(500).json({ error:"New " + error.message });
+  }
+});
+
+app.post('/claim', async (req, res) => {
+
+
+  res.header('Access-Control-Allow-Origin', 'https://ashikurrahman25.github.io'); // Allow your specific origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  try {
+
+    console.log(req.body)
+    const { receiver_id, amount, memo } = req.body;
+
+
+    if (!receiver_id) {
+      return res.status(400).json({ error: 'Missing "greeting" in the request body.' });
+    }
+
+      
+  const near = await connect(
+    testConfig
+  );
+
+    const acc = await near.account('splaunch.testnet');
+    const functionCallResult = await acc.functionCall({
+      contractId: 'splaunch.testnet',
+      methodName: 'ft_transfer',
+      args: {
+        receiver_id,
+        amount,
+        memo
+      },
+      gas: BigInt (100000000000000), // attached GAS (optional)
+      attachedDeposit: BigInt(1) // attached deposit in yoctoNEAR (optional)
+    });
+  
+
+    // Call the contract's set_greeting method
+    // const result = await (contract as any)['ft_transfer'](
+    // {
+    //   receiver_id: receiver_id,
+    //   amount: amount,
+    //   memo:memo
+    // }
+    // ,"10000000000000"
+    // ,"1"
+
+    // );
+
+
+
+  // // Extract transaction hash from the function call result
+  // const transactionHash = functionCallResult.transaction.hash;
+
+  // // Extract transaction status using the transaction hash
+  // const transactionStatus = await near.connection.provider.txStatus(transactionHash, acc.accountId);
+
+  // // Extract and log the contract execution logs
+  // const logs = transactionStatus.receipts_outcome.flatMap(outcome => outcome.outcome.logs);
+
+  // // Log the transaction receipt
+  // const receipt = transactionStatus.transaction_outcome;
+
+  // return {
+  //   logs,
+  //   receipt,
+  // };
+
+  console.log(functionCallResult.transaction_outcome.id)
+  res.json({ success: true, message: "Successfully claimed reward!",txnLink:`https://testnet.nearblocks.io/txns/${functionCallResult.transaction_outcome.id}` });
+
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
   }
 });
 
