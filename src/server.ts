2b8a3f7695627398305fd7d133e71af0a49587b8
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-06-13 22:12:05 +0600

    CORS

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const myKeyStore = new keyStores.InMemoryKeyStore();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors({ origin: ['http://localhost:3000', 'https://nearvidia.com'] }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://nearvidia.com', 'https://ashikurrahman25.github.io'] }));
 
 const { connect } = nearAPI;
 
